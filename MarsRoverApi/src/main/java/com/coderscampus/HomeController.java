a3f9e940f48f57a7716e725b42201a675f827e02
Runningghost <runningghost16@gmail.com>
2022-07-11 22:54:03 -0400

    Adding initial project

---
+++ MarsRoverApi/src/main/java/com/coderscampus/HomeController.java
@@ -0,0 +1,18 @@
+package com.coderscampus;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String getHomeView (ModelMap model) {
+
+		return "index";
+	}
+
+
+}
