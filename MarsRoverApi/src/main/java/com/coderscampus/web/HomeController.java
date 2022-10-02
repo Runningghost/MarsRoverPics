3a2b9ec0210d9fff90122af2f828f32a523c9a63
macgr <macgr@kmacbutterfly.attlocal.net>
2022-10-02 17:24:31 -0400

    more

--- MarsRoverApi/src/main/java/com/coderscampus/web/HomeController.java
+++ MarsRoverApi/src/main/java/com/coderscampus/web/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String getHomeView(ModelMap model, @RequestParam(required=false) String marsApiRoverData,
-		@RequestParam(required = false) Integer marsSol) {
+		@RequestParam(required=false) Integer marsSol) {
 		// if request param is empty, then set a default value
 		if (StringUtils.isEmpty(marsApiRoverData)) {
 			marsApiRoverData = "opportunity";
